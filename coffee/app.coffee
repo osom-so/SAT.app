99b5d41bc99a2f28ad44216f2dd07ed6b2f5aec6
Rafael Soto <fael.soto@gmail.com>
2013-08-03 20:56:40 -0500

    Views: Bugfix de transición cuando hacemos scroll y volvemos

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -5,6 +5,19 @@ $ ->
   SAT.currentView = 0
   SAT.history = []
 
+  $('#app, #app2').bind 'webkitAnimationEnd', (e)->
+    if ~['slideout', 'slideout-back'].indexOf e.originalEvent.animationName
+      $out = $(@)
+      $in = $("#app, #app2").filter(":not(##{$(@).attr('id')})")
+      $out.empty()
+      $in.css
+        top: 0
+  .bind 'webkitAnimationStart', (e)->
+    if ~['slidein', 'slidein-back'].indexOf e.originalEvent.animationName
+      $in = $(@)
+      $in.css
+        top: window.scrollY
+
   Backbone.history.bind 'route', ->
     if SAT.history.length > 1 and @getFragment() is SAT.history[-2..][0]
       SAT.history.pop()
@@ -30,6 +43,24 @@ $ ->
   ### Models ###
   ### Collections ###
   ### Views ###
+  class SAT.headerView extends Backbone.View
+    el: '#app-header'
+    template: _.template $('#tmpl-header').html()
+    events:
+      'click a.back': 'evt_back'
+      'click a.notif': 'evt_notif'
+      'click a.help': 'evt_help'
+    evt_back: (e)->
+      e.preventDefault()
+      router.navigate SAT.history[-2..][0], true
+    evt_notif: (e)->
+      e.preventDefault()
+    evt_help: (e)->
+      e.preventDefault()
+    render: ->
+      @$el.html @template
+      @
+
   class SAT.loginView extends Backbone.AnimView
     el: '#app'
     template: _.template $('#tmpl-login').html()
@@ -46,8 +77,20 @@ $ ->
   class SAT.indexView extends Backbone.AnimView
     el: '#app'
     template: _.template $('#tmpl-index').html()
+    events:
+      'click a': 'evt_menuitem'
+    evt_menuitem: (e)->
+      e.preventDefault()
+      router.navigate $(e.currentTarget).attr('href'), true
     render: ->
       @$el.html @template
+      @
+
+  class SAT.citasView extends Backbone.AnimView
+    el: '#app'
+    template: _.template $('#tmpl-citas').html()
+    render: ->
+      @$el.html @template(url: window.location.hash)
       @
 
   ### Router ###
@@ -55,6 +98,10 @@ $ ->
     routes:
       '': 'index'
       'login': 'login'
+      'citas': 'citas'
+      'pagos': 'citas'
+      'notificaciones': 'citas'
+      'ayuda': 'citas'
     index: ->
       return @navigate 'login', true unless SAT.isLogged
       indexview = new SAT.indexView
@@ -64,5 +111,12 @@ $ ->
       loginview = new SAT.loginView
       loginview.render()
 
+    citas: ->
+      citasview = new SAT.citasView
+      citasview.render()
+
   router = new SAT.Router
   Backbone.history.start()
+
+  header = new SAT.headerView
+  header.render()
--- js/app.js
+++ js/app.js
@@ -3,12 +3,31 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   $(function() {
-    var router, _ref, _ref1, _ref2, _ref3;
+    var header, router, _ref, _ref1, _ref2, _ref3, _ref4, _ref5;
     window.SAT = {};
     SAT.isLogged = false;
     SAT.goBack = false;
     SAT.currentView = 0;
     SAT.history = [];
+    $('#app, #app2').bind('webkitAnimationEnd', function(e) {
+      var $in, $out;
+      if (~['slideout', 'slideout-back'].indexOf(e.originalEvent.animationName)) {
+        $out = $(this);
+        $in = $("#app, #app2").filter(":not(#" + ($(this).attr('id')) + ")");
+        $out.empty();
+        return $in.css({
+          top: 0
+        });
+      }
+    }).bind('webkitAnimationStart', function(e) {
+      var $in;
+      if (~['slidein', 'slidein-back'].indexOf(e.originalEvent.animationName)) {
+        $in = $(this);
+        return $in.css({
+          top: window.scrollY
+        });
+      }
+    });
     Backbone.history.bind('route', function() {
       if (SAT.history.length > 1 && this.getFragment() === SAT.history.slice(-2)[0]) {
         SAT.history.pop();
@@ -54,12 +73,51 @@
 
     /* Views*/
 
+    SAT.headerView = (function(_super) {
+      __extends(headerView, _super);
+
+      function headerView() {
+        _ref1 = headerView.__super__.constructor.apply(this, arguments);
+        return _ref1;
+      }
+
+      headerView.prototype.el = '#app-header';
+
+      headerView.prototype.template = _.template($('#tmpl-header').html());
+
+      headerView.prototype.events = {
+        'click a.back': 'evt_back',
+        'click a.notif': 'evt_notif',
+        'click a.help': 'evt_help'
+      };
+
+      headerView.prototype.evt_back = function(e) {
+        e.preventDefault();
+        return router.navigate(SAT.history.slice(-2)[0], true);
+      };
+
+      headerView.prototype.evt_notif = function(e) {
+        return e.preventDefault();
+      };
+
+      headerView.prototype.evt_help = function(e) {
+        return e.preventDefault();
+      };
+
+      headerView.prototype.render = function() {
+        this.$el.html(this.template);
+        return this;
+      };
+
+      return headerView;
+
+    })(Backbone.View);
     SAT.loginView = (function(_super) {
       __extends(loginView, _super);
 
       function loginView() {
-        _ref1 = loginView.__super__.constructor.apply(this, arguments);
-        return _ref1;
+        _ref2 = loginView.__super__.constructor.apply(this, arguments);
+        return _ref2;
       }
 
       loginView.prototype.el = '#app';
@@ -88,13 +146,22 @@
       __extends(indexView, _super);
 
       function indexView() {
-        _ref2 = indexView.__super__.constructor.apply(this, arguments);
-        return _ref2;
+        _ref3 = indexView.__super__.constructor.apply(this, arguments);
+        return _ref3;
       }
 
       indexView.prototype.el = '#app';
 
       indexView.prototype.template = _.template($('#tmpl-index').html());
+
+      indexView.prototype.events = {
+        'click a': 'evt_menuitem'
+      };
+
+      indexView.prototype.evt_menuitem = function(e) {
+        e.preventDefault();
+        return router.navigate($(e.currentTarget).attr('href'), true);
+      };
 
       indexView.prototype.render = function() {
         this.$el.html(this.template);
@@ -104,19 +171,45 @@
       return indexView;
 
     })(Backbone.AnimView);
+    SAT.citasView = (function(_super) {
+      __extends(citasView, _super);
+
+      function citasView() {
+        _ref4 = citasView.__super__.constructor.apply(this, arguments);
+        return _ref4;
+      }
+
+      citasView.prototype.el = '#app';
+
+      citasView.prototype.template = _.template($('#tmpl-citas').html());
+
+      citasView.prototype.render = function() {
+        this.$el.html(this.template({
+          url: window.location.hash
+        }));
+        return this;
+      };
+
+      return citasView;
+
+    })(Backbone.AnimView);
     /* Router*/
 
     SAT.Router = (function(_super) {
       __extends(Router, _super);
 
       function Router() {
-        _ref3 = Router.__super__.constructor.apply(this, arguments);
-        return _ref3;
+        _ref5 = Router.__super__.constructor.apply(this, arguments);
+        return _ref5;
       }
 
       Router.prototype.routes = {
         '': 'index',
-        'login': 'login'
+        'login': 'login',
+        'citas': 'citas',
+        'pagos': 'citas',
+        'notificaciones': 'citas',
+        'ayuda': 'citas'
       };
 
       Router.prototype.index = function() {
@@ -134,11 +227,19 @@
         return loginview.render();
       };
 
+      Router.prototype.citas = function() {
+        var citasview;
+        citasview = new SAT.citasView;
+        return citasview.render();
+      };
+
       return Router;
 
     })(Backbone.Router);
     router = new SAT.Router;
-    return Backbone.history.start();
+    Backbone.history.start();
+    header = new SAT.headerView;
+    return header.render();
   });
 
 }).call(this);
